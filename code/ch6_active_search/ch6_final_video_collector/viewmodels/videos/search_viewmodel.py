c16acb7c102bf7aba329f1134cebeed715aafbf2
Michael Kennedy <mikeckennedy@gmail.com>
2021-09-21 12:12:29 -0700

    Now doing the search on the server.

--- code/ch6_active_search/ch6_final_video_collector/viewmodels/videos/search_viewmodel.py
+++ code/ch6_active_search/ch6_final_video_collector/viewmodels/videos/search_viewmodel.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from models.video_model import Video
+from services import video_service
 from viewmodels.shared.viewmodelbase import ViewModelBase
 
 
@@ -6,3 +10,7 @@ class SearchViewModel(ViewModelBase):
         super().__init__()
 
         self.search_text: str = self.request_dict.get('search_text')
+        self.videos: List[Video] = []
+
+        if self.search_text and self.search_text.strip():
+            self.videos = video_service.search_videos(self.search_text)
