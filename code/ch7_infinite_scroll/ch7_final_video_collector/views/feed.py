7209ae8c38e5daec440b5edced45a0ebaa39fd45
Michael Kennedy <mikeckennedy@gmail.com>
2021-09-22 11:11:06 -0700

    Infinite scroll now working on the video feed.

--- code/ch7_infinite_scroll/ch7_final_video_collector/views/feed.py
+++ code/ch7_infinite_scroll/ch7_final_video_collector/views/feed.py
@@ -1,3 +1,5 @@
+import time
+
 import flask
 
 from infrastructure.view_modifiers import response
@@ -12,4 +14,12 @@ VIDEOS_PER_PAGE = 3
 @response(template_file='feed/index.html')
 def index():
     vm = FeedViewModel(page_size=VIDEOS_PER_PAGE)
+    return vm.to_dict()
+
+
+@blueprint.get('/feed/more_videos/<int:page>')
+@response(template_file='feed/partials/video_list.html')
+def more_videos(page: int):
+    time.sleep(.5)
+    vm = FeedViewModel(page_size=VIDEOS_PER_PAGE, page=page)
     return vm.to_dict()
